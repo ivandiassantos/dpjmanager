191e9992f80120c4221546db6dfebeebf794715d
antonio.sousa <antonio.sousa@192.168.56.1>
2016-11-04 17:05:13 -0200

    Construção da funcionalidade de faturamento.

---
+++ codigo-fonte/dpjmanager/src/main/java/br/com/dpjmanager/controller/FaturamentoController.java
@@ -0,0 +1,26 @@
+package br.com.dpjmanager.controller;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.dpjmanager.enums.URLRetorno;
+
+
+@Controller
+@Scope(value = WebApplicationContext.SCOPE_REQUEST)
+@RequestMapping("/restrito/faturamentos")
+public class FaturamentoController {
+	
+	@RequestMapping(value = "/iniciarPesquisaFaturamento", method=RequestMethod.GET)
+	public ModelAndView iniciarPesquisaFaturamento(){
+		
+		
+		return new ModelAndView(URLRetorno.PAGINA_PESQUISA_FATURAMENTO.getUrl());
+		
+		
+	}
+}
--- codigo-fonte/dpjmanager/src/main/java/br/com/dpjmanager/enums/URLRetorno.java
+++ codigo-fonte/dpjmanager/src/main/java/br/com/dpjmanager/enums/URLRetorno.java
@@ -17,7 +17,8 @@ public enum URLRetorno
 {
    PAGINA_AUTENTICACAO("/login"),
       REDIRECT_URL_LOGIN("redirect:/login"),
-      PAGINA_PRINCIPAL("/restrito/principal");
+      PAGINA_PRINCIPAL("/restrito/principal"),
+   	  PAGINA_PESQUISA_FATURAMENTO("/restrito/faturamentos/pesquisaFaturamento");
 
    @Getter
    private String url;
